edf12e512cc947dcb6866f0894756bb689839fc9
Scott Tolley <michaeltolley21@hotmail.com>
2022-02-27 19:17:13 +0000

    Testing GitHub Action

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,12 +2,14 @@
 
 echo "Starting HippoTech 2.0 React front end"
 output=$(mktemp "${TMPDIR:-/tmp/}$(basename 0).XXX")
-npm start
+#npm start
 npm start  &> $output &
 server_pid=$!
 echo "Server pid: $server_pid"
 echo "Output: $output"
 echo "Wait:"
+sleep 30
+cat $output
 until grep -q -i 'You can now view hippotech-react in the browser.' $output
 do
   if ! ps $server_pid > /dev/null
