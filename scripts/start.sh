ea956e9b0ee3b7618e6c77afb94001c0ee39230a
Scott Tolley <michaeltolley21@hotmail.com>
2022-02-27 19:08:33 +0000

    Adding GitHub action to run tests in CI

---
+++ scripts/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Starting HippoTech 2.0 React front end"
+output=$(mktemp "${TMPDIR:-/tmp/}$(basename 0).XXX")
+npm start  &> $output &
+server_pid=$!
+echo "Server pid: $server_pid"
+echo "Output: $output"
+echo "Wait:"
+until grep -q -i 'You can now view hippotech-react in the browser.' $output
+do
+  if ! ps $server_pid > /dev/null
+  then
+    echo "The server died" >&2
+    exit 1
+  fi
+  echo -n "."
+  sleep 1
+done
+echo
+echo "HippoTech front-end is running."
